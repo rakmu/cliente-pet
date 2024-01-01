098163155ddff89b2d4c9c6da0cba96503ad849b
Ramon Barbosa <ramon@ramonbarbosa.com>
2024-01-01 12:07:44 -0300

    implementa getTodos

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
@@ -2,10 +2,13 @@ package br.com.petz.clientepet.cliente.application.api;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import br.com.petz.clientepet.cliente.domain.Cliente;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
 
+@Value
 public class ClienteListResponse {
 
 	private UUID idCliente;
@@ -14,8 +17,20 @@ public class ClienteListResponse {
 	private String cpf;
 	private String email;
 	private String celular;
+	
 	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-		// TODO Auto-generated method stub
-		return null;
+		return clientes.stream()
+				.map(ClienteListResponse::new)
+				.collect(Collectors.toList());
 	}
+
+	private ClienteListResponse(Cliente cliente) {
+		this.idCliente = cliente.getIdCliente();
+		this.nomeCompleto = cliente.getNomeCompleto();
+		this.cpf = cliente.getCpf();
+		this.email = cliente.getEmail();
+		this.celular = cliente.getCelular();
+	}
+	
+	
 }
