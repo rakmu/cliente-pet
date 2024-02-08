aa6809a5a3eef39887721ab3e38378888fbb3ae3
Ramon Barbosa <ramon@ramonbarbosa.com>
2024-02-08 19:01:40 -0300

    implementa camada API

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -41,4 +41,11 @@ public class ClienteController implements ClienteAPI {
 		return clienteDetalhado;
 	}
 
+	@Override
+	public void deletaClienteAtravesId(UUID idCliente) {
+		log.info("[inicia] ClienteController deletaClienteAtravesId");
+		log.info("[idCliente] {}", idCliente);
+		log.info("[finaliza] ClienteController deletaClienteAtravesId");
+	}
+
 }
